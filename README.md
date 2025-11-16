9329c1fba70e33456e2f0636bb8937ec9e9e9813
lunolunoluno <noellucien2001@gmail.com>
2025-11-16 16:04:35 +0100

    Result tab UI created

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # VocaPractice
 
-This project requires a [Google Gemini API Key](https://aistudio.google.com/app/api-keys).
+## Requirements
+
+- **Python version >3.10**
+- [A Google Gemini API Key](https://aistudio.google.com/app/api-keys)
+
+### Setup
+
+It is recommended to create a Python [virtual environment](https://docs.python.org/3/library/venv.html)
+```sh
+python3 -m venv .venv
+source .venv/bin/activate       # macOS / Linux
+# or
+.\.venv\Scripts\activate        # Windows
+```
+
+Then install dependencies:
+
+```sh
+pip install -r requirements.txt
+```
+
+# Run
+
+Start the application with:
+
+```sh
+python main.py
+```
+
+After launching, Gradio will print a local URL in the terminal (typically http://127.0.0.1:7860).
+Open this link in your browser to access the app.
--- app_UI.py
+++ app_UI.py
@@ -1,6 +1,7 @@
 import gradio as gr
 
 from typing import List
+from difflib import Differ
 from sentence_generator import SentenceGenerator
 from utils import get_nb_sentences, get_target_language
 
@@ -13,14 +14,39 @@ class AppUI:
 
     def generate_sentences(self)->List[str]:
         sentences = self.sg.generate_sentences()
-        for i, s in enumerate(sentences):
-            self.target_sentences[i] = s[get_target_language()]
-            self.reference_sentences[i] = s["english"]
+        for i in range(get_nb_sentences()):
+            self.target_sentences[i] = sentences[i]["english"]
+            self.reference_sentences[i] = sentences[i][get_target_language()]
         return self.target_sentences
+    
+
+    def compare_all_sentences(self, *answers):
+        d = Differ()
+        results = []
+
+        for i, user_answer in enumerate(answers):
+            reference = self.reference_sentences[i]
+            diff = [
+                (token[2:], token[0] if token[0] != " " else None)
+                for token in d.compare(user_answer.split(), reference.split())
+            ]
+            diff = [d for d in diff if d[1] in [None, "+", "-"]]
+            results.append(diff)
+
+        return results
+
 
     def launch_ui(self)->None:
         sentences_markdowns = [gr.Markdown() for _ in range(get_nb_sentences())]
         sentences_answers = [gr.Textbox() for _ in range(get_nb_sentences())]
+        results_highlight = [gr.HighlightedText(
+            label="Your Answer",
+            show_legend=True,
+            color_map={
+                "+": "green",
+                "-": "red"
+            }
+        ) for _ in range(get_nb_sentences())]
 
         def menu_ui()->gr.Blocks:
             with gr.Blocks() as interface:
@@ -41,13 +67,24 @@ class AppUI:
                             gr.Markdown(f"## Sentence {i+1}")
                             sentences_markdowns[i].render()
                             sentences_answers[i].render()
-                gr.Button("Check Answers")
+                check_answers_btn = gr.Button("Check Answers")
 
+                check_answers_btn.click(
+                    fn=self.compare_all_sentences,
+                    inputs=sentences_answers,
+                    outputs=results_highlight
+                )
             return interface
         
         def results_ui()->gr.Blocks:
             with gr.Blocks() as interface:
                 gr.Markdown("Here are your results!")
+                with gr.Column():
+                    for i in range(get_nb_sentences()):
+                        with gr.Group():
+                            gr.Markdown(f"## Sentence {i+1}")
+                            results_highlight[i].render()
+
             return interface
         
 
